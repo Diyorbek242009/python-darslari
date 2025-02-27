9eadd55f4aba489121c690c6934cf46d7de69d23
Diyorbek.artificial intelligence <meliyevsohib563@gmail.com>
2025-02-27 22:26:07 +0500

    Update 1-dars.py

--- python/1-dars/1-dars.py
+++ python/1-dars/1-dars.py
@@ -5,11 +5,11 @@ Spyder Editor
 This is a temporary script file.
 """
 
-#Hello word
+Hello word
 
-#print("Hello world")
-#print(9+8)
+print("Hello world")
+print(9+8)
 
-#print('Assalom alaykum')
-#print("Hayrli tong!")
-#print(19/5)
+print('Assalom alaykum')
+print("Hayrli tong!")
+print(19/5)
